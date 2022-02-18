9443242972fd4f3656d08785a02f6c39c6ffcafb
Danilo Quesada <danilo.q@CEBMAC0004.local>
2022-02-19 07:51:34 +0800

    Update: spike react-dnd

---
+++ src/components/Droppable/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { DroppableProps } from './types'
+
+const Droppable: React.FC<DroppableProps> = () => <div>Droppable</div>
+
+export default Droppable
---
+++ src/components/Droppable/types.ts
@@ -0,0 +1,3 @@
+export interface DroppableProps {
+  droppableId: string
+}
