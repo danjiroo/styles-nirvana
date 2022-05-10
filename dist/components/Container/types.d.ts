c53b5f4bd68767ec0fbf6833b6a6768b9d57819d
eugenepeter <eugene.m@pandolink.com>
2022-05-10 19:45:19 +0800

    build

--- dist/components/Container/types.d.ts
+++ dist/components/Container/types.d.ts
@@ -22,7 +22,6 @@ export interface ContainerProps extends GlobalSizes {
     centerY?: boolean;
     bgColor?: string;
     customBg?: string;
-    height?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 import { ThemeProvider } from '../../themes';
 export interface GetContainerStyles extends ThemeProvider, GlobalSizes {
